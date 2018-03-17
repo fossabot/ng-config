4b994f244dd966528107cc61c0d42b84e7bf2556
Moe Myat Zaw <mmzliveid@gmail.com>
2018-03-17 21:33:09 +0630

    Initial Commit

--- modules/ng-config-cache-http-loader/src/config-cache-http-loader.ts
+++ modules/ng-config-cache-http-loader/src/config-cache-http-loader.ts
@@ -1,12 +1,16 @@
+import { isPlatformBrowser } from '@angular/common';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Inject, Injectable, InjectionToken, Optional } from '@angular/core';
+import { Inject, Injectable, InjectionToken, Optional, PLATFORM_ID } from '@angular/core';
 
 import { CacheEntryOptions, CacheService, handleCacheResponse } from '@bizappframework/ng-cache';
-import { ConfigLoader } from '@bizappframework/ng-config';
+import { CONFIG_STATIC_DATA, ConfigLoader, ConfigStaticData } from '@bizappframework/ng-config';
 import { CONFIG_ENDPOINT_URL } from '@bizappframework/ng-config/http-loader';
+import { of } from 'rxjs/observable/of';
 import { map } from 'rxjs/operators';
 
 export interface ConfigCacheHttpLoaderOptions {
+    loadOnBrowserOnly?: boolean;
+    skipLoadOnStaticData?: boolean;
     cacheConfigKey?: string;
     endpointUrl?: string | (() => string);
 }
@@ -17,10 +21,20 @@ export const CONFIG_CACHE_HTTP_LOADER_OPTIONS = new InjectionToken<ConfigCacheHt
 export class ConfigCacheHttpLoader implements ConfigLoader {
     private readonly _cacheConfigKey: string = 'app.config';
     private readonly _endpoint: string = '/appsettings.json';
+    private readonly _disableLoad: boolean = false;
 
     constructor(private readonly _cacheService: CacheService, private readonly _httpClient: HttpClient,
+        @Inject(PLATFORM_ID) platformId: Object,
         @Optional() @Inject(CONFIG_CACHE_HTTP_LOADER_OPTIONS) options?: ConfigCacheHttpLoaderOptions,
-        @Optional() @Inject(CONFIG_ENDPOINT_URL) endpointUrl?: string) {
+        @Optional() @Inject(CONFIG_ENDPOINT_URL) endpointUrl?: string,
+        @Optional() @Inject(CONFIG_STATIC_DATA) staticData?: ConfigStaticData) {
+        if (options && options.loadOnBrowserOnly && !isPlatformBrowser(platformId)) {
+            this._disableLoad = true;
+        }
+        if (options && options.skipLoadOnStaticData && (!staticData || !Object.keys(staticData).length)) {
+            this._disableLoad = true;
+        }
+
         if (options && options.endpointUrl) {
             this._endpoint = typeof options.endpointUrl === 'string' ? options.endpointUrl : options.endpointUrl();
         } else if (endpointUrl) {
@@ -39,6 +53,10 @@ export class ConfigCacheHttpLoader implements ConfigLoader {
     async load(): Promise<{ [key: string]: any }> {
         return this._cacheService.getOrSet(this._cacheConfigKey,
             (entryOptions: CacheEntryOptions) => {
+                if (this._disableLoad) {
+                    return of({});
+                }
+
                 return this._httpClient.get(this._endpoint, { observe: 'response' }).pipe(map(
                     (response: HttpResponse<{ [key: string]: any }>) => {
                         return handleCacheResponse<{ [key: string]: any }>(response,
